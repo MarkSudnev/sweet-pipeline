efcbc3b2012a0a5772efb08ccadcccf156624736
MarkSudnev <m.sudnev@gmail.com>
2025-04-27 01:12:39 +0200

    update

--- data-source/src/app.py
+++ data-source/src/app.py
@@ -1,5 +1,6 @@
 
 import json
+import subprocess
 from flask import Flask
 from flask import request
 from flask import Response
@@ -13,7 +14,11 @@ def data():
     if len(q) != 1:
         return Response(f"Wrong Request: {request.query_string}", status=400)
     _, query = q[0].split("=")
-    return json.dumps({
-        "query": query,
-        "response": "some response data"
-    })
+    result = subprocess.run(
+        [f"/opt/llama2.c/run /opt/llama2.c/stories15M.bin -z /opt/llama2.c/tokenizer.bin -n 256 -i \"{query}\""],
+        capture_output=True,
+        shell=True,
+        text=True
+    )
+    response_body = json.dumps({"query": query, "response": f"{result.stdout} | {result.stderr}"})
+    return Response(response_body, status=200, content_type="application/json")
