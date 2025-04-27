18db67a20a580a20a1bc45dd6f7b0b0ec2ee259e
MarkSudnev <m.sudnev@gmail.com>
2025-04-27 23:43:44 +0200

    update

--- data-source/src/app.py
+++ data-source/src/app.py
@@ -9,11 +9,9 @@ app = Flask(__name__)
 
 @app.route("/data", methods=["GET"])
 def data():
-    query_parts = request.query_string.decode().split("&")
-    q = list(filter(lambda a: a.startswith("q="), query_parts))
-    if len(q) != 1:
-        return Response(f"Wrong Request: {request.query_string}", status=400)
-    _, query = q[0].split("=")
+    query = request.args.get("q")
+    if not query:
+        return Response("No query found", status=400)
     result = subprocess.run(
         [f"/opt/llama2.c/run /opt/llama2.c/stories15M.bin -z /opt/llama2.c/tokenizer.bin -n 256 -i \"{query}\""],
         capture_output=True,
