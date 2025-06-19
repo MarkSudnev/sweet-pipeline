654131f512449f12666da3298f18758524d222d2
Markel Sudnev <m.sudnev@gmail.com>
2025-06-19 19:02:34 +0200

    add simple kafka consumer

--- data-consumer/main/main.py
+++ data-consumer/main/main.py
@@ -1,7 +1,9 @@
-
+from kafka import KafkaConsumer
 
 def main():
-  pass
+  consumer = KafkaConsumer('sweet', bootstrap_servers=['localhost:9092'])
+  for message in consumer:
+    print(message)
 
 if __name__ == "__main__":
   main()
