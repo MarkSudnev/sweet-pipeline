90e09c0df2226584b350fdfd9eac2c021e5cc5b2
Markel Sudnev <m.sudnev@gmail.com>
2025-06-19 23:06:40 +0200

    set up kafka connections

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Generates data by query
 ## TODO
 Add Kafka event publishing to minio
 [minio-kafka manual](https://min.io/docs/minio/linux/administration/monitoring/publish-events-to-kafka.html)
+
+#### Debug commands
+```bash
+./bin/kafka-topics.sh --bootstrap-server=localhost:9092 --list
+./bin/kafka-console-consumer.sh --bootstrap-server=localhost:9092 --topic=sweet --from-beginning
+```
--- data-consumer/main/main.py
+++ data-consumer/main/main.py
@@ -1,9 +1,17 @@
 from kafka import KafkaConsumer
 
+
 def main():
-  consumer = KafkaConsumer('sweet', bootstrap_servers=['localhost:9092'])
+  consumer = KafkaConsumer(
+    'sweet',
+    bootstrap_servers=['localhost:29092'],
+    # auto_offset_reset='earliest',
+    # enable_auto_commit=True
+  )
+  print("Listening", consumer.topics())
   for message in consumer:
     print(message)
+
 
 if __name__ == "__main__":
   main()
